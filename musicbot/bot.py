67e893e2af6aea435b9453cf591276f23bf2f121
Willi Ye <williye97@gmail.com>
2017-09-26 17:01:15 +0200

    Iterate through autoplaylist

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -19,7 +19,7 @@ from io import BytesIO
 from functools import wraps
 from textwrap import dedent
 from datetime import timedelta
-from random import choice, shuffle
+from random import shuffle, randint
 from collections import defaultdict
 
 from musicbot.playlist import Playlist
@@ -77,6 +77,7 @@ class MusicBot(discord.Client):
 
         self.blacklist = set(load_file(self.config.blacklist_file))
         self.autoplaylist = load_file(self.config.auto_playlist_file)
+        self.autoplaylistCount = randint(0, len(self.autoplaylist))
         self.downloader = downloader.Downloader(download_folder='audio_cache')
 
         self.exit_signal = None
@@ -416,7 +417,10 @@ class MusicBot(discord.Client):
     async def on_player_finished_playing(self, player, **_):
         if not player.playlist.entries and not player.current_entry and self.config.auto_playlist:
             while self.autoplaylist:
-                song_url = choice(self.autoplaylist)
+                song_url = self.autoplaylist[self.autoplaylistCount]
+                self.autoplaylistCount += 1
+                if self.autoplaylistCount >= len(self.autoplaylist):
+                    self.autoplaylistCount = 0
                 info = await self.downloader.safe_extract_info(player.playlist.loop, song_url, download=False, process=False)
 
                 if not info:
